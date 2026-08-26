9b54bdd05cede74e868c5ad89219a38ed9b25626
galihcandraa <galihcandrakirana17@gmail.com>
2026-08-26 12:00:08 +0700

    feat: add ResponseStatusException handler

--- src/main/java/com/github/galihcandraa/personal_planner/exception/GlobalExceptionHandler.java
+++ src/main/java/com/github/galihcandraa/personal_planner/exception/GlobalExceptionHandler.java
@@ -3,17 +3,23 @@ package com.github.galihcandraa.personal_planner.exception;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ResponseStatusException;
 
 import jakarta.validation.ValidationException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-    
+
     @ExceptionHandler(ValidationException.class)
     public ResponseEntity<String> handleValidation(ValidationException e) {
         return ResponseEntity.badRequest().body(e.getMessage());
     }
-    
+
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<String> handleResponseStatus(ResponseStatusException e) {
+        return ResponseEntity.status(e.getStatusCode()).body(e.getReason());
+    }
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleOther(Exception e) {
         return ResponseEntity.internalServerError().body("Terjadi kesalahan " + e.getMessage());
